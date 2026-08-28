e6c337f9c2d6e4e439c8a444980273883fe99931
Gevorg <hovhannisyangevorg930@gmail.com>
2026-08-28 21:19:26 +0400

    terraform fmt maked

--- task04/main.tf
+++ task04/main.tf
@@ -83,12 +83,12 @@ resource "azurerm_network_interface" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
-ip_configuration {
-  name                          = var.nic_name
-  subnet_id                     = azurerm_subnet.main.id
-  private_ip_address_allocation = "Dynamic"
-  public_ip_address_id          = azurerm_public_ip.main.id
-}
+  ip_configuration {
+    name                          = var.nic_name
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
 
   tags = {
     Creator = var.student_email

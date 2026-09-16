3509d3833e09efb1ad1fc46a656188ca44a108e8
Gevorg <hovhannisyangevorg930@gmail.com>
2026-09-17 02:41:46 +0400

    changing outputs on task 08_b

--- task08_b/modules/aca/outputs.tf
+++ task08_b/modules/aca/outputs.tf
@@ -1,9 +1,9 @@
 output "fqdn" {
-  description = "Public FQDN of the Azure Container App."
-  value       = azurerm_container_app.this.ingress[0].fqdn
+  description = "FQDN of the latest revision of the Azure Container App."
+  value       = azurerm_container_app.this.latest_revision_fqdn
 }
 
 output "identity_id" {
-  description = "Resource ID of the user-assigned identity used by the Container App."
+  description = "Resource ID of the user-assigned identity used by the Azure Container App."
   value       = azurerm_user_assigned_identity.this.id
 }
